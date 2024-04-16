33d6b266b63a077afbc58061b9efe176e7863121
iber <i-ber@mail.ru>
2024-04-16 19:36:46 +0300

    starting admin dev

---
+++ utils/check_password.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import os
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == os.getenv("CHROMA_ADMIN_PASS"):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
